789728dea1d5bc8a5355d73e7da0388dd39078a6
Mikhail Tsedilin <tsedilin.mi@edu.spbstu.ru>
2025-06-24 00:24:23 +0300

    lab3 try 2 with MEMPC and MSTATUS

--- src/tb/scr1_tb_log_cmd.sv
+++ src/tb/scr1_tb_log_cmd.sv
@@ -5,10 +5,10 @@ always_ff @(posedge scr1_top_tb_ahb.i_top.i_imem_ahb.clk) begin
 
     if (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_resp == 2'b01) begin
 
-        // valid data from ahb router
+        // valid jal data
         if (scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[6 : 0] == 7'b1101111) begin
-            // detect and command
-            $display("rd = %h, imm = %h", scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[11 : 7], scr1_top_tb_ahb.i_top.i_imem_ahb.imem_rdata[31 : 25]);
+            // csr_mepc and csr_mstatus
+            $display("0x341  MEPC = %h,  0x300  MSTATUS = %h", scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.i_pipe_csr.csr_mepc[31 : 0], scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.i_pipe_csr.csr_mstatus[31 : 0]);
         end
     end
 end
